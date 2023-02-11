15ce350d18a79d4927282a99e986bf19a6d1a0d9
Toby Law <toby@tzcl.me>
2023-02-11 21:37:45 +1100

    2023 restart

--- static/js/preload.js
+++ static/js/preload.js
@@ -5,8 +5,6 @@ const { ipcRenderer, contextBridge, shell, clipboard, webFrame } = require('elec
 const IS_MAC = process.platform === 'darwin'
 const IS_WIN32 = process.platform === 'win32'
 
-const ALLOWED_EXTERNAL_PROTOCOLS = ['https:', 'http:', 'mailto:', 'zotero:', 'file:']
-
 function getFilePathFromClipboard () {
   if (IS_WIN32) {
     const rawFilePath = clipboard.read('FileNameW')
@@ -65,10 +63,6 @@ contextBridge.exposeInMainWorld('apis', {
   },
 
   async openExternal (url, options) {
-    const protocol = new URL(url).protocol
-    if (!ALLOWED_EXTERNAL_PROTOCOLS.includes(protocol)) {
-      throw new Error('illegal protocol')
-    }
     await shell.openExternal(url, options)
   },
 
@@ -78,7 +72,7 @@ contextBridge.exposeInMainWorld('apis', {
 
   showItemInFolder (fullpath) {
     if (IS_WIN32) {
-      shell.openPath(path.dirname(fullpath).replaceAll("/", "\\"))
+      shell.openPath(path.dirname(fullpath))
     } else {
       shell.showItemInFolder(fullpath)
     }
@@ -89,12 +83,11 @@ contextBridge.exposeInMainWorld('apis', {
    *
    * @param {string} html html file with embedded state
    */
-  exportPublishAssets (html, customCSSPath, exportCSSPath, repoPath, assetFilenames, outputDir) {
+  exportPublishAssets (html, customCSSPath, repoPath, assetFilenames, outputDir) {
     ipcRenderer.invoke(
       'export-publish-assets',
       html,
       customCSSPath,
-      exportCSSPath,
       repoPath,
       assetFilenames,
       outputDir
@@ -116,7 +109,11 @@ contextBridge.exposeInMainWorld('apis', {
 
     const dest = path.join(repoPathRoot, to)
     const assetsRoot = path.dirname(dest)
-    
+
+    if (!/assets$/.test(assetsRoot)) {
+      throw new Error('illegal assets dirname')
+    }
+
     await fs.promises.mkdir(assetsRoot, { recursive: true })
 
     from = from && decodeURIComponent(from || getFilePathFromClipboard())
@@ -168,10 +165,6 @@ contextBridge.exposeInMainWorld('apis', {
 
   setZoomFactor (factor) {
     webFrame.setZoomFactor(factor)
-  },
-
-  setZoomLevel (level) {
-    webFrame.setZoomLevel(level)
   },
 
   isAbsolutePath: path.isAbsolute.bind(path)
